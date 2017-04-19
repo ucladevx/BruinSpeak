7cda4cbdba91c5202efddefb06337b9a5777f592
Cody Ley-Han <codyleyhan@gmail.com>
2017-04-18 17:48:11 -0700

    user model extended

---
+++ db/migrate/20170419004335_add_profile_info_columns_to_users.rb
@@ -0,0 +1,8 @@
+class AddProfileInfoColumnsToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :first_name, :string, null: false
+    add_column :users, :last_name, :string, null: false
+    add_column :users, :major, :string
+    add_column :users, :organizations, :text
+  end
+end
