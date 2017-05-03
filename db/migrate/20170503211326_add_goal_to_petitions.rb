eb27b6ec18633872926a87961c17df279e686a4c
Omar Ozgur <oozgur217@gmail.com>
2017-05-03 14:47:00 -0700

    Added default to goal migration

--- db/migrate/20170503211326_add_goal_to_petitions.rb
+++ db/migrate/20170503211326_add_goal_to_petitions.rb
@@ -1,5 +1,5 @@
 class AddGoalToPetitions < ActiveRecord::Migration[5.0]
   def change
-    add_column :petitions, :goal, :integer, null: false
+    add_column :petitions, :goal, :integer, null: false, default: 1
   end
 end
