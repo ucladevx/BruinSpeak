e5992f3c0effcbd20e20cbd11dc82151668073d2
Cody Ley-Han <codyleyhan@gmail.com>
2017-05-06 20:41:41 -0700

    fixed the edit page for UX

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -13,17 +13,8 @@ $(function() {
   })()
 })
 
-function clearPicture(e) {
-  e.preventDefault();
-  let file = document.getElementById("user_profile_pic");
-
-  file.value = null;
-  let el = document.getElementById("file-upload-edit");
-  el.style.display = "none";
-}
-
 function handleProfileUpload() {
-  let preview = document.getElementById("profile-pic-preview");
+  let preview = document.getElementById("profile-pic-edit");
   let files = this.files;
   let file = files[0];
   let imageType = /^image\//;
@@ -37,15 +28,11 @@ function handleProfileUpload() {
   img.classList.add("profile-preview");
   img.file = file;
   img.width = 150;
-  preview.insertBefore(img, preview.childNodes[0]);
+  img.alt = "profile picture";
+  preview.removeChild(preview.firstElementChild)
+  preview.append(img);
 
   let reader = new FileReader();
   reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
   reader.readAsDataURL(file);
-
-  let el = document.getElementById("file-upload-edit");
-  el.style.display = "block";
-
-  let btn = document.getElementById("clear-picture");
-  btn.onclick = clearPicture;
 }
