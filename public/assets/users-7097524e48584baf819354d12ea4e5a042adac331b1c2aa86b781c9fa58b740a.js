238073ec4d8ee619696d49abe463c42b0efac4e0
Omar Ozgur <oozgur217@gmail.com>
2017-05-10 19:20:45 -0700

    Attempted to fix heroku again

---
+++ public/assets/users-7097524e48584baf819354d12ea4e5a042adac331b1c2aa86b781c9fa58b740a.js
@@ -0,0 +1,45 @@
+$(function() {
+  (function (){
+    var el = document.getElementById("profile_upload");
+    var file = document.getElementById("user_profile_pic");
+
+    var clear = document.getElementById("button-clear");
+    clear.onclick = function(e) {
+      e.preventDefault();
+      location.reload();
+    }
+
+    el.onclick = function(e) {
+      e.preventDefault();
+      file.click();
+    }
+
+    var fileSelect = document.getElementById("user_profile_pic");
+    fileSelect.addEventListener("change", handleProfileUpload, false);
+  })()
+})
+
+function handleProfileUpload() {
+  var preview = document.getElementById("profile-pic-edit");
+  var files = this.files;
+  var file = files[0];
+  var imageType = /^image\//;
+
+  if(!imageType.test(file.type)) {
+    return;
+  }
+
+  var img = document.createElement("img");
+  img.classList.add("img-circle");
+  img.classList.add("profile-preview");
+  img.file = file;
+  img.width = 150;
+  img.alt = "profile picture";
+  preview.removeChild(preview.firstElementChild)
+  preview.append(img);
+
+  var reader = new FileReader();
+  reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
+  reader.readAsDataURL(file);
+}
+;
