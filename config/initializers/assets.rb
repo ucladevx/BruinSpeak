1c7387927ae162663a4b212951a0c5fa6a2a375d
Omar Ozgur <oozgur217@gmail.com>
2017-05-11 18:55:05 -0700

    Attempted fix for header and footer issue

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( explore.js )
+Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb *.png *.jpg *.svg )
+
