dab6c54723a74f351e63dee1bde042970c23c731
kluangkote <katiel97@hotmail.com>
2017-06-07 16:53:11 -0700

    Fix dropdown for petition tag

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,9 +1,12 @@
+var mouse_inside = false;
+
 var dropDown = function() {
   'use strict';
 
   $('.dropdown').click(function(e){
      e.stopPropagation();
       $('#dropdown-content').toggleClass('selected');
+
   });
 };
 
@@ -13,4 +16,15 @@ document.addEventListener("turbolinks:load", function() {
 
 $(document).click(function(){
   $("#dropdown-content").removeClass('selected');
+  if (!mouse_inside) {
+    $("#status-dropdown-content").removeClass('selected');
+  }
+});
+
+$(document).ready(function() {
+  $('#status-dropdown').hover(function() {
+    mouse_inside = true;
+  }, function() {
+    mouse_inside = false;
+  });
 });
