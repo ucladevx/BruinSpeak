df6af2578f2479effa35bb4d05ec609b6d32d919
Taasin <taasin.saquib@gmail.com>
2017-11-15 13:43:44 -0800

    Updated counters on home page

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -3,9 +3,18 @@ $(".card").on("click", function(){
   console.log("clicked");
 });
 
-jQuery(document).ready(function($) {
-  $('.counter').counterUp({
-      delay: 10,
-      time: 1000
-    });
-});
+// update counters on home page
+var odometer1 = document.getElementById("odometer1");
+var odometer2 = document.getElementById("odometer2");
+
+var count1 = document.getElementById("count1");
+var count2 = document.getElementById("count2");
+
+setTimeout(function(){
+    // odometer1.innerHTML = count1.innerHTML;
+    // odometer2.innerHTML = count2.innerHTML;
+
+    // hard coded for now
+    odometer1.innerHTML = 576;
+    odometer2.innerHTML = 1738;
+}, 1000);
