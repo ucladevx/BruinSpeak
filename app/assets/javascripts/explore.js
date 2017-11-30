af8dc74b273b7abaa84892b77b92dc0c72b504d7
brianc725 <brianc725@gmail.com>
2017-11-29 20:24:42 -0800

    creates bubbles w/ text and colors

--- app/assets/javascripts/explore.js
+++ app/assets/javascripts/explore.js
@@ -30,50 +30,113 @@
 //https://github.com/vlandham/bubble_chart/blob/gh-pages/src/bubble_chart.js
 
 var maxsize = 120;
-var x = 250;
-var y = 150;
-
-var svg;
+//var x = 250;
+//var y = 150;
 var count = 0;
-var rowCounter = 0;
+var svg;
+//var count = 0;
+//var rowCounter = 0;
 
-var colorPick = 0;
+//var colorPick = 0;
 
 /*
 todo: randomize locations of the bubbles
 make sure they dont overlap
-
-how to read in the text, all examples online use csv???
-figure out the window size restriction in the csss
 */
 
- var data = new Object();
- var nodes = [];
+ //var data = new Object();
+ var info = [];
 
 function jsonify(name, url) {
-	var obj = {};
+	//var obj = {};
+    var obj = new Object();
 
+    obj.x = 45+(count*100);  //FIGURE OUT X AND Y 
+    obj.y = 50;
     obj.name = name;
     obj.url = url;
-    console.log(obj);
+    obj.r = maxsize;
 
-    nodes.push(obj);
-    console.log(nodes);
+    if (count < 3) 
+    {
+      obj.color = "292E49";
+    }
+    else 
+    {
+      obj.color = "11998e";
+    }
+    //console.log(obj);
 
-    data.nodes = nodes;
-    console.log(data);
+    info.push(obj);
+    //console.log(info);
+
+   // data.info = info;
+   // console.log(data);
+
+    nextBubble();
 }
 
-function getRandomColor() {
-	/*
-     var colours = ["#00c0f1", "#add036", "#ec2426", "#ffc116", "#FFF95F", "#83B8FD"];
-     return colours[Math.floor(Math.random() * 6)]
-     */
+function nextBubble() {
+	if (maxsize > 80)
+ 	{
+		maxsize = maxsize - 10; //make the next bubble smaller
+	}
+  count++;
+}
+
+
+function getRandomColor(num) {
+    console.log(num);
 
      var colors = ["#292E49",  "11998e"];
-     return colors[colorPick];
 
+     if (num < 3)
+     {
+      return colors[0];
+     }
+     return colors[1];
+     num++;
  }
+
+ var width = 1100;
+ var height = 1100;
+
+function createBubbles() {
+  console.log(info);
+ 
+
+ var svg = d3.select("svg")
+ 	.attr("width", window.innerWidth)
+ 	.attr("height", height)
+  .attr("class", "bubble");
+ //	.attr("transform", "translate(0,0)");
+
+ var nodes = svg.selectAll("circle").data(info);
+
+ nodes.enter().append("circle")
+  .attr("cx", function(d){ return d.x; })
+  .attr("cy", function(d){ return d.y; }); 
+
+ nodes.attr("r", 0)
+  .style("fill", function(d){return d.color;})
+  .attr("r", function(d){return d.r;});
+
+  nodes.enter().append("text")
+        .attr("x", function(d){ return d.x; })
+        .attr("y", function(d){ return d.y; })
+        .attr("text-anchor", "middle")
+        .text(function(d){ return d.name; })
+        .style({
+            "fill":"white", 
+            "font-family":"Helvetica, sans-serif",
+            "font-size": "12px"
+        });
+
+ 
+}
+
+
+/*
 
 function makeBubble(name, url) {
 
@@ -86,7 +149,6 @@ if (count == 0)
 	.append("g")
 	.attr("transform", "translate(0,0)");
 }
-//	alert(name);
 
 	var circle = svg.append("circle")
 		.attr("cx", x)
@@ -95,9 +157,9 @@ if (count == 0)
 		.attr('class', 'click-circle')
 		.attr("xlink:href", url)
 		.attr("r", maxsize);
-		/*
-		.attr("stroke", "black")
-     	.attr("stroke-width", 2); */
+		
+		//.attr("stroke", "black")
+     	//.attr("stroke-width", 2); 
 
      	if (count == 2)
      	{
@@ -125,10 +187,12 @@ if (count == 0)
      	else if (maxsize >= 80)
      	{
      	var text = svg.append("text") 	//add text
-   			.attr("dx", x-((1/3)*maxsize))
+   			//.attr("dx", x-((1/3)*maxsize))
+   			.attr("dx", x)
    			.attr("dy", y)
    			.attr("font-size", "12px")
    			.attr("fill", "black")  	//black text everywhere else
+   			.attr("text-anchor", "middle")
    			.text(name);
      		x+=maxsize*2;
   			if (rowCounter == 4)	//new row, push bubbles down 1
@@ -161,23 +225,6 @@ svg.on('click', function() {
         //alert("works");
     });
 
-/*
-var circle = svg.append("circle")
-	.attr("cx", 30)
-    .attr("cy", 30)
-     .attr("r", 20)
-     .style("fill", "blue");
-
-var circle2 = svg.append("circle")
-	.attr("cx", 90)
-    .attr("cy", 90)
-     .attr("r", 50);
 
 
-var circle3 = svg.append("circle")
-	.attr("cx", 300)
-	.attr("cy", 210)
-	.style("fill", "red")
-	.attr("r", 200);
-
-	*/
+*/
