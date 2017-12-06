0cc61317d8e2bd806198e546613a67ad6d5932ba
Omar Ozgur <oozgur217@gmail.com>
2017-12-06 14:57:45 -0800

    Fix counters when loading through Turbolinks

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -3,18 +3,19 @@ $(".card").on("click", function(){
   console.log("clicked");
 });
 
-// update counters on home page
-var odometer1 = document.getElementById("odometer1");
-var odometer2 = document.getElementById("odometer2");
+document.addEventListener("turbolinks:load", function() {
+  // update counters on home page
+  var odometer1 = document.getElementById("odometer1");
+  var odometer2 = document.getElementById("odometer2");
 
-var count1 = document.getElementById("count1");
-var count2 = document.getElementById("count2");
-
-setTimeout(function(){
+  var count1 = document.getElementById("count1");
+  var count2 = document.getElementById("count2");
+  setTimeout(function(){
     // odometer1.innerHTML = count1.innerHTML;
     // odometer2.innerHTML = count2.innerHTML;
 
     // hard coded for now
     odometer1.innerHTML = 576;
     odometer2.innerHTML = 1738;
-}, 1000);
+  }, 1000);
+});
